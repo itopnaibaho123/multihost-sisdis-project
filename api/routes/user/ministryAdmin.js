3e02359f37134bccbdf3c1ee92a5f2509838ae74
mikaelalvianrizky <mikael.alvian.rizky@gmail.com>
2024-03-15 23:15:40 +0700

    finally the template are finished!

---
+++ api/routes/user/ministryAdmin.js
@@ -0,0 +1,10 @@
+const ministryAdminRouter = require('express').Router()
+const ministryAdminController = require('../../controllers/user/ministryAdmin.js')
+
+ministryAdminRouter.get('/', ministryAdminController.getList)
+ministryAdminRouter.get('/:ministryAdminId', ministryAdminController.getById)
+ministryAdminRouter.post('/', ministryAdminController.create)
+ministryAdminRouter.put('/:ministryAdminId', ministryAdminController.update)
+ministryAdminRouter.delete('/:ministryAdminId', ministryAdminController.remove)
+
+module.exports = ministryAdminRouter;
