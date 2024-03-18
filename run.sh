d11cec163303c49d20c36b6ff09fc766c6ee3790
steven <ssteven075@gmail.com>
2024-03-18 21:21:22 +0700

    update script

--- run.sh
+++ run.sh
@@ -9,5 +9,6 @@ sleep 5
 sleep 5
 
 CC_NAME="basic"
+CC_FOLDER="atcontract"
 
-./network.sh deployCC -ccn $CC_NAME -ccp ../chaincode/atcontract -ccl go
+./network.sh deployCC -ccn $CC_NAME -ccp ../chaincode/$CC_FOLDER -ccl go
