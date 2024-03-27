d2eae7b570f7044d5da1a518a3f46370e095f224
mikaelalvianrizky <mikael.alvian.rizky@gmail.com>
2024-03-27 20:35:11 +0700

    rapihin try catch user.js, sekarang full dipindah ke service

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -3,41 +3,30 @@ const iResp = require('../utils/response.interface.js')
 const userService = require('../services/user.js')
 
 const enrollAdmin = async (req, res) => {
-  try {
-    const data = req.body
-    const username = data.username
-    const password = data.password
-    const orgName = data.organizationName
+  const data = req.body
+  const username = data.username
+  const password = data.password
+  const orgName = data.organizationName
 
-    const result = await userService.enrollAdmin(username, password, orgName)
-    res.status(200).send(result)
-  } catch (error) {
-    res
-      .status(500)
-      .send(iResp.buildErrorResponse(500, 'Something wrong', error))
-  }
+  const result = await userService.enrollAdmin(username, password, orgName)
+
+  res.status(result.code).send(result)
 }
 
 const registerAdminKementrian = async (req, res) => {
-  try {
-    const data = req.body
-    const username = data.username
-    const email = data.email
-    const orgName = data.organizationName
-    const role = data.role
+  const data = req.body
+  const username = data.username
+  const email = data.email
+  const orgName = data.organizationName
+  const role = data.role
 
-    const result = await userService.registerAdminKementrian(
-      username,
-      email,
-      orgName,
-      role
-    )
-    res.status(result.code).send(result)
-  } catch (error) {
-    res
-      .status(500)
-      .send(iResp.buildErrorResponse(500, 'Something wrong', error))
-  }
+  const result = await userService.registerAdminKementrian(
+    username,
+    email,
+    orgName,
+    role
+  )
+  res.status(result.code).send(result)
 }
 
 const registerUser = async (req, res) => {
@@ -58,42 +47,31 @@ const registerUser = async (req, res) => {
 }
 
 const loginUser = async (req, res) => {
-  try {
-    const data = req.body
-    const username = data.username
-    const password = data.password
+  const data = req.body
+  const username = data.username
+  const password = data.password
 
-    const result = await userService.loginUser(username, password)
-    res.status(result.code).send(result)
-  } catch (error) {
-    res
-      .status(500)
-      .send(iResp.buildErrorResponse(500, 'Something wrong', error))
-  }
+  const result = await userService.loginUser(username, password)
+  res.status(result.code).send(result)
 }
 
 const updateUser = async (req, res) => {
-  try {
-    const data = req.body
-    const username = data.username
-    const password = data.password
-    const organizationName = data.organizationName
-    const dataUser = data.dataUser
-    const role = data.role
+  const data = req.body
+  const username = data.username
+  const password = data.password
+  const organizationName = data.organizationName
+  const dataUser = data.dataUser
+  const role = data.role
 
-    const result = await userService.updateUser(
-      organizationName,
-      username,
-      password,
-      role,
-      dataUser
-    )
-    res.status(200).send(result)
-  } catch (error) {
-    res
-      .status(500)
-      .send(iResp.buildErrorResponse(500, 'Something wrong', error))
-  }
+  const result = await userService.updateUser(
+    organizationName,
+    username,
+    password,
+    role,
+    dataUser
+  )
+
+  res.status(result.code).send(result)
 }
 
 module.exports = {
