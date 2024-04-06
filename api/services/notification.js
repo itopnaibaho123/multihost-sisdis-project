9c505316247e624ccba33188c15479b349afede7
mikaelalvianrizky <mikael.alvian.rizky@gmail.com>
2024-04-07 02:13:44 +0700

    fixing notification error in admin-perusahaan role

--- api/services/notification.js
+++ api/services/notification.js
@@ -3,141 +3,142 @@ const fabric = require('../utils/fabric.js')
 const { bufferToJson } = require('../utils/converter.js')
 
 const getNotification = async (user) => {
-  try {
-    if (
-      user.userType === 'admin-kementerian' ||
-      user.userType === 'staf-kementerian'
-    ) {
-      // Status Pending =>  SupplyChain
-      const supplyChain = await fabric.connectToNetwork(
-        user.organizationName,
-        'sccontract',
-        user.username
-      )
-      const supplyChainList = await supplyChain.contract.submitTransaction(
-        'GetAllSCByStatus',
-        'pending'
-      )
-      supplyChain.gateway.disconnect()
+  // try {
+  if (
+    user.userType === 'admin-kementerian' ||
+    user.userType === 'staf-kementerian'
+  ) {
+    // Status Pending =>  SupplyChain
+    const supplyChain = await fabric.connectToNetwork(
+      user.organizationName,
+      'sccontract',
+      user.username
+    )
+    const supplyChainList = await supplyChain.contract.submitTransaction(
+      'GetAllSCByStatus',
+      'pending'
+    )
+    supplyChain.gateway.disconnect()
 
-      // Status Pending => CSP
-      const CarbonSalesProposal = await fabric.connectToNetwork(
-        user.organizationName,
-        'cspontract',
-        user.username
-      )
-      const cspList = await CarbonSalesProposal.contract.submitTransaction(
-        'GetAllCSPByStatus',
-        'pending'
-      )
-      supplyChain.gateway.disconnect()
+    // Status Pending => CSP
+    const CarbonSalesProposal = await fabric.connectToNetwork(
+      user.organizationName,
+      'cspontract',
+      user.username
+    )
+    const cspList = await CarbonSalesProposal.contract.submitTransaction(
+      'GetAllCSPByStatus',
+      'pending'
+    )
+    supplyChain.gateway.disconnect()
 
-      // Company ApprovalStatus => 0
-      const Company = await fabric.connectToNetwork(
-        user.organizationName,
-        'pecontract',
-        user.username
-      )
-      const companyList = await Company.contract.submitTransaction(
-        'ReadAllPendingPerusahaan'
-      )
-      Company.gateway.disconnect()
+    // Company ApprovalStatus => 0
+    const Company = await fabric.connectToNetwork(
+      user.organizationName,
+      'pecontract',
+      user.username
+    )
+    const companyList = await Company.contract.submitTransaction(
+      'ReadAllPendingPerusahaan'
+    )
+    Company.gateway.disconnect()
 
-      const result = {
-        supplyChain: supplyChainList,
-        carbonSalesProposal: cspList,
-        company: companyList,
-      }
-      return iResp.buildSuccessResponse(
-        200,
-        'Successfully get all Notification',
-        JSON.parse(result)
-      )
-    } else if (user.userType === 'admin-perusahaan') {
-      // Status Pending => Carbon Transaction
-      const carbonTransactionNet = await fabric.connectToNetwork(
-        user.organizationName,
-        'ctcontract',
-        user.username
-      )
-      const carbonTransactionPerusahaan =
-        await carbonTransactionNet.contract.submitTransaction(
-          'GetCTbyIdPerusahaan',
-          user.idPerusahaan
-        )
-      carbonTransactionNet.gateway.disconnect()
-
-      const carbonTransactionQuery = carbonTransactionPerusahaan.filter(
-        function (item) {
-          return item.status == 'pending'
-        }
-      )
-
-      // Status Menunggu Persetujuan Perusahaan => supply Chain
-      const companyNet = await fabric.connectToNetwork(
-        user.organizationName,
-        'pecontract',
-        user.username
-      )
-      let company = await companyNet.contract.submitTransaction(
-        'GetPerusahaanById',
+    const result = {
+      supplyChain: supplyChainList,
+      carbonSalesProposal: cspList,
+      company: companyList,
+    }
+    return iResp.buildSuccessResponse(
+      200,
+      'Successfully get all Notification',
+      JSON.parse(result)
+    )
+  } else if (user.userType === 'admin-perusahaan') {
+    // Status Pending => Carbon Transaction
+    const carbonTransactionNet = await fabric.connectToNetwork(
+      user.organizationName,
+      'ctcontract',
+      user.username
+    )
+    const carbonTransactionPerusahaan =
+      await carbonTransactionNet.contract.submitTransaction(
+        'GetCTbyIdPerusahaan',
         user.idPerusahaan
       )
-      companyNet.gateway.disconnect()
-      company = bufferToJson(company)
+    carbonTransactionNet.gateway.disconnect()
 
-      const supplyChainNet = await fabric.connectToNetwork(
-        user.organizationName,
-        'sccontract',
-        user.username
-      )
-      const listSupplyChain = company.supplyChain
-
-      const transactionResults = await Promise.all(
-        listSupplyChain.map(async (idSupplyChain) => {
-          const result = await companyNet.contract.submitTransaction(
-            'GetSCById',
-            idSupplyChain
-          )
-          return result
-        })
-      )
-
-      const result = {
-        carbonTransaction: carbonTransactionQuery,
-        supplyChain: transactionResults.filter(function (item) {
-          return item.status == 'menunggu persetujuan perusahaan'
-        }),
+    const carbonTransactionQuery = carbonTransactionPerusahaan.filter(
+      function (item) {
+        return item.status == 'pending'
       }
-      return iResp.buildSuccessResponse(
-        200,
-        'Successfully get all Notification',
-        JSON.parse(result)
-      )
-    } else {
-      const network = await fabric.connectToNetwork(
-        user.organizationName,
-        'shcontract',
-        user.username
-      )
-      const shipment = await network.contract.submitTransaction(
-        'GetShipmentsNeedApprovalByDivisiPenerima',
-        user.idDivisi
-      )
-      network.gateway.disconnect()
-      console.log(shipment)
-      const result = {
-        shipment: bufferToJson(shipment),
-      }
-      return iResp.buildSuccessResponse(
-        200,
-        'Successfully get all Notification',
-        result
-      )
+    )
+
+    // Status Menunggu Persetujuan Perusahaan => supply Chain
+    const companyNet = await fabric.connectToNetwork(
+      user.organizationName,
+      'pecontract',
+      user.username
+    )
+    let company = await companyNet.contract.submitTransaction(
+      'GetPerusahaanById',
+      user.idPerusahaan
+    )
+    companyNet.gateway.disconnect()
+    company = bufferToJson(company)
+
+    const supplyChainNet = await fabric.connectToNetwork(
+      user.organizationName,
+      'sccontract',
+      user.username
+    )
+    const listSupplyChain = company.supplyChain
+
+    const transactionResults = await Promise.all(
+      listSupplyChain.map(async (idSupplyChain) => {
+        const result = await companyNet.contract.submitTransaction(
+          'GetSCById',
+          idSupplyChain
+        )
+        return result
+      })
+    )
+
+    const result = {
+      carbonTransaction: bufferToJson(carbonTransactionQuery),
+      supplyChain: transactionResults.filter(function (item) {
+        return item.status == 'menunggu persetujuan perusahaan'
+      }),
     }
-  } catch (error) {
-    return iResp.buildErrorResponse(500, 'Something wrong', error.message)
+    console.log(result)
+    return iResp.buildSuccessResponse(
+      200,
+      'Successfully get all Notification',
+      result
+    )
+  } else {
+    const network = await fabric.connectToNetwork(
+      user.organizationName,
+      'shcontract',
+      user.username
+    )
+    const shipment = await network.contract.submitTransaction(
+      'GetShipmentsNeedApprovalByDivisiPenerima',
+      user.idDivisi
+    )
+    network.gateway.disconnect()
+    console.log(shipment)
+    const result = {
+      shipment: bufferToJson(shipment),
+    }
+    return iResp.buildSuccessResponse(
+      200,
+      'Successfully get all Notification',
+      result
+    )
   }
+  // } catch (error) {
+  //   return iResp.buildErrorResponse(500, 'Something wrong', error.message)
+  // }
 }
 
 module.exports = { getNotification }
