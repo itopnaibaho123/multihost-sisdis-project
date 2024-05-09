700476df4438af8f164162d662114cacece4c378
itopnaibaho123 <christopher.naibaho14@gmail.com>
2024-05-09 22:56:14 +0700

    updating on docker yaml

--- down.sh
+++ down.sh
@@ -2,5 +2,4 @@ cd network
 
 ./network.sh down
 
-docker volume ls --format '{{.Name}}' | grep '^compose_' | awk '{print $1}' | xargs docker volume rm
-docker network rm supply_chain_carbon
+docker volume ls --format '{{.Name}}' | grep '\.com' | awk '{print $1}' | xargs docker volume rm
