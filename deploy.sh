93eb72f26d1aa7e12ee6cc130078ea81ec65340a
steven2801 <ssteven075@gmail.com>
2024-05-12 20:21:10 +0700

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
+# ./deploy.sh cecontract h1
+
 ARG=$1
+CCSTEP=$2
+
 
 cd network
 
-./network.sh deployCC -ccn $ARG -ccp ../chaincode/$ARG -ccl go
+./network.sh deployCC -ccn $ARG -ccp ../chaincode/$ARG -ccl go --ccstep $CCSTEP 
