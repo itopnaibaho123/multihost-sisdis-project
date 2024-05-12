fa09a854c76062593ff553adf89c3977e5348064
steven2801 <ssteven075@gmail.com>
2024-05-12 17:16:07 +0700

    update

--- api/utils/fabric.js
+++ api/utils/fabric.js
@@ -44,10 +44,10 @@ const connectToNetwork = async (organizationName, chaincodeName, user) => {
   await gateway.connect(ccp, {
     wallet,
     identity: user,
-    discovery: { 
-      enabled: true,
-      asLocalhost: false
-     },
+    // discovery: { 
+    //   enabled: true,
+    //   asLocalhost: false
+    //  },
     // https://stackoverflow.com/questions/56936560/why-do-i-take-more-than-2-seconds-to-just-do-a-transaction
     eventHandlerOptions: {
       strategy: null,
