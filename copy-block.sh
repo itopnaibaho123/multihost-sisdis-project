fbecb5e75017e92451e613660566744fbbbaf602
steven2801 <ssteven075@gmail.com>
2024-05-12 13:10:20 +0700

    update

--- copy-block.sh
+++ copy-block.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PATH_CHANNEL_BLOCK='/home/backend/channel-artifacts'
+PATH_CHANNEL_BLOCK='/home/backend/network/channel-artifacts'
 
 gcloud compute scp --recurse $PATH_CHANNEL_BLOCK instance-supplychain1:$PATH_CHANNEL_BLOCK
 gcloud compute scp --recurse $PATH_CHANNEL_BLOCK instance-supplychain2:$PATH_CHANNEL_BLOCK
